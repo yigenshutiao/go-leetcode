79c0d5bec9c89471707a3d8f40f492eff9367064
gaoxing <richardgaoxing@didichuxing.com>
2020-03-14 23:50:12 +0800

    451. 根据字符出现频率排序

---
+++ 0451-SortCharactersByFrequency/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func frequencySort(s string) string {
+	if len(s) <= 0 {
+		return ""
+	}
+
+	// 把字符串读到map里面
+	m := map[byte]int{}
+	for i := 0; i < len(s); i++ {
+		if m[s[i]] > 0 {
+			m[s[i]] += 1
+		} else {
+			m[s[i]] = 1
+		}
+	}
+
+	var countArr []int
+	for _, v := range m {
+		countArr = append(countArr, v)
+	}
+
+	// 这个语法真丑
+	sort.Sort(sort.Reverse(sort.IntSlice(countArr)))
+
+	res := ""
+	for _, v := range countArr {
+		for k := range m {
+			if v == m[k] {
+				fmt.Println(v, m[k], k)
+				t := ""
+				for i := 0; i < v; i++ {
+					t += string(k)
+				}
+				res += t
+				delete(m, k)
+
+			}
+		}
+	}
+
+	return res
+}
+
+func main() {
+	frequencySort("tree")
+}
