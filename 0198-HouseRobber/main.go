6366e869a964e4bb5face2ae28bb2a7c8858aade
Xing Gao <18340825824@163.com>
2020-03-24 09:02:40 +0800

    198. 打家劫舍

---
+++ 0198-HouseRobber/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	}
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		if dp[i] > res {
+			res = dp[i]
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(rob([]int{2, 7, 9, 3, 1}))
+}
